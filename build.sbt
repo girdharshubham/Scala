202a9ecfcb0a94779b591273a403ca8718278d72
Shubham Girdhar <girdharshubham@hotmail.com>
2023-08-18 17:08:13 +0530

    chore: add actors intro

--- build.sbt
+++ build.sbt
@@ -6,13 +6,13 @@ lazy val root = (project in file("."))
 lazy val voyager = (project in file("voyager"))
   .settings(
     scalaVersion := "3.3.0",
-    description := "Scala 3"
+    description  := "Scala 3",
   )
 
 lazy val discovery = (project in file("discovery"))
   .settings(
     scalaVersion := "2.13.11",
-    description := "Scala 2",
+    description  := "Scala 2",
     resolvers += "Akka library repository".at("https://repo.akka.io/maven"),
-    libraryDependencies ++= akka
+    libraryDependencies ++= akka,
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val AkkaVersion = "2.8.4"
+  lazy val AkkaVersion           = "2.8.4"
+  lazy val LogbackClassicVersion = "1.2.12"
 
   lazy val `akka-typed` =
     "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
@@ -9,5 +10,7 @@ object Dependencies {
   lazy val `akka-typed-testkit` =
     "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test
 
-  lazy val akka = Seq(`akka-typed`, `akka-typed-testkit`)
+  lazy val logback = "ch.qos.logback" % "logback-classic" % LogbackClassicVersion
+
+  lazy val akka = Seq(`akka-typed`, `akka-typed-testkit`, logback)
 }
